7b5b206827ee2359d2efb381db082745896085d9
Agata <kozinska.agata@gmail.com>
2019-02-07 21:56:24 +0100

    Add desktop version and fix animation in menu

--- 1. menu/js/index.js
+++ 1. menu/js/index.js
@@ -1,11 +1,16 @@
 var button = document.querySelector('.hamburger__button');
+var buttonClose = 'hamburger__button--close'; 
 var menu = document.querySelector('.menu__wrapper');
 var menuVisible = 'menu__wrapper--visible';
 
 
-button.addEventListener('click', e => { 
-  button.classList.toggle('hamburger__button--close');
+button.addEventListener('click', () => { 
+  button.classList.toggle(buttonClose);
   menu.classList.toggle(menuVisible);
+
+  if (!(button.classList.contains(buttonClose))) {
+    sublist.classList.remove(sublistOpen);
+  }
 });
 
 document.addEventListener('click', e => {
@@ -13,7 +18,7 @@ document.addEventListener('click', e => {
   var navBar = document.querySelector('.header__menu')
 
   if (e.target === background || e.target === navBar ) {
-    button.classList.remove('hamburger__button--close');
+    button.classList.remove(buttonClose);
     menu.classList.remove(menuVisible);
   }
 });
@@ -21,9 +26,21 @@ document.addEventListener('click', e => {
 var listItems = document.querySelectorAll('.nav__list__item');
 listItems = [...listItems];
 var firstItem = listItems[0];
-var sublist = document.querySelector('.nav__sublist')
+var sublist = document.querySelector('.nav__sublist');
+var sublistOpen = 'nav__sublist--open';
 
 firstItem.addEventListener('click', () => { 
   firstItem.classList.toggle('nav__list__item--active');
-  sublist.classList.toggle('nav__sublist--open');
+  sublist.classList.toggle(sublistOpen);
 });
+
+window.addEventListener('resize', () => {
+  if (window.screen.width >= 1200) {
+    if (sublist.classList.contains(sublistOpen)) {
+      button.classList.remove(buttonClose);
+      menu.classList.remove(menuVisible);
+      firstItem.classList.remove('nav__list__item--active');
+      sublist.classList.remove(sublistOpen);
+    }
+  }
+})
