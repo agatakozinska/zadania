b534a75dcb59e37b705aef7d328b623a80ddb780
Agata <kozinska.agata@gmail.com>
2019-02-07 14:01:58 +0100

    Finish mobile version

--- 1. menu/js/index.js
+++ 1. menu/js/index.js
@@ -1,9 +1,19 @@
 var button = document.querySelector('.hamburger__button');
-var nav = document.querySelector('.nav');
-var navVisible = 'nav--visible';
+var menu = document.querySelector('.menu__wrapper');
+var menuVisible = 'menu__wrapper--visible';
 
 button.addEventListener('click', () => { 
   //sprobowac z this
   button.classList.toggle('hamburger__button--close');
-  nav.classList.toggle(navVisible);
+  menu.classList.toggle(menuVisible);
+});
+
+var listItems = document.querySelectorAll('.nav__list__item');
+listItems = [...listItems];
+var firstItem = listItems[0];
+var sublist = document.querySelector('.nav__sublist')
+
+firstItem.addEventListener('click', () => { 
+  firstItem.classList.toggle('nav__list__item--active');
+  sublist.classList.toggle('nav__sublist--open');
 });
