0a236258a90ffcabc0df93163057475b49643971
Agata <kozinska.agata@gmail.com>
2019-02-11 10:44:27 +0100

    Final ver for paint; fix hover in menu

--- 1. menu/js/index.js
+++ 1. menu/js/index.js
@@ -1,41 +1,43 @@
-var button = document.querySelector('.hamburger__button');
-var buttonClose = 'hamburger__button--close'; 
-var menu = document.querySelector('.menu__wrapper');
-var menuVisible = 'menu__wrapper--visible';
+const button = document.querySelector('.hamburger__button');
+const buttonClose = 'hamburger__button--close';
+const menu = document.querySelector('.menu__wrapper');
+const menuVisible = 'menu__wrapper--visible';
+let listItems = document.querySelectorAll('.nav__list__item');
+listItems = [...listItems];
+const firstItem = listItems[0];
+const sublist = document.querySelector('.nav__sublist');
+const sublistOpen = 'nav__sublist--open';
 
-
-button.addEventListener('click', () => { 
+button.addEventListener('click', () => {
   button.classList.toggle(buttonClose);
   menu.classList.toggle(menuVisible);
 
   if (!(button.classList.contains(buttonClose))) {
     sublist.classList.remove(sublistOpen);
+    firstItem.classList.remove('nav__list__item--active');
   }
 });
 
-document.addEventListener('click', e => {
-  var background = document.querySelector('.header')
-  var navBar = document.querySelector('.header__menu')
+document.addEventListener('click', event => {
+  const background = document.querySelector('.header')
+  const navBar = document.querySelector('.header__menu')
 
-  if (e.target === background || e.target === navBar ) {
+  if (event.target === background || event.target === navBar) {
     button.classList.remove(buttonClose);
     menu.classList.remove(menuVisible);
   }
 });
 
-var listItems = document.querySelectorAll('.nav__list__item');
-listItems = [...listItems];
-var firstItem = listItems[0];
-var sublist = document.querySelector('.nav__sublist');
-var sublistOpen = 'nav__sublist--open';
-
-firstItem.addEventListener('click', () => { 
+firstItem.addEventListener('click', event => {
+  event.preventDefault();
   firstItem.classList.toggle('nav__list__item--active');
   sublist.classList.toggle(sublistOpen);
 });
 
+
 window.addEventListener('resize', () => {
-  if (window.screen.width >= 1200) {
+  if (window.screen.innerWidth >= 1200) {
+
     if (sublist.classList.contains(sublistOpen)) {
       button.classList.remove(buttonClose);
       menu.classList.remove(menuVisible);
@@ -43,4 +45,4 @@ window.addEventListener('resize', () => {
       sublist.classList.remove(sublistOpen);
     }
   }
-})
+});
